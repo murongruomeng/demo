9e94ffda14f4031eafafb12a2a0d176b99cdfabf
Alex <murongruomeng@gmail.com>
2014-12-10 17:47:20 +0800

    test

--- algorithm/main/java/demo/alex/searching/Hashing.java
+++ algorithm/main/java/demo/alex/searching/Hashing.java
@@ -4,4 +4,8 @@ package demo.alex.searching;
  * Created by admin on 2014年12月10日 0010.
  */
 public class Hashing {
+
+    public static void main(String[] args){
+        System.out.println("Hello, Maven！");
+    }
 }
