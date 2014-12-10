ceedcb8ba22eb0010792d64ffa5e3d18fae9bedb
Alex <murongruomeng@gmail.com>
2014-12-10 17:39:14 +0800

    test

---
+++ algorithm/main/java/demo/alex/searching/Hashing.java
@@ -0,0 +1,7 @@
+package demo.alex.searching;
+
+/**
+ * Created by admin on 2014年12月10日 0010.
+ */
+public class Hashing {
+}
---
+++ algorithm/test/java/alex/demo/test/TestApp.java
@@ -0,0 +1,7 @@
+package alex.demo.test;
+
+/**
+ * Created by admin on 2014年12月10日 0010.
+ */
+public class TestApp {
+}
